8ddd1c8856dc67bbf294736bc6d4b187f5c41676
Joel Douglass <joel.douglass@velir.com>
2018-04-20 12:53:14 -0400

    Remove double boxing

--- client/examples/04 - Box/02 - Compose.js
+++ client/examples/04 - Box/02 - Compose.js
@@ -6,15 +6,10 @@ const splitAndBox = s => R.compose(
   Box,
   R.split(s));
 
-// joinAndBox :: String → [String] -> Box(String)
-const joinAndBox = s => R.compose(
-  Box,
-  R.join(s));
-
 // toSlug :: String → Box(Box(String))
 const toSlug = R.compose(
   R.toLower,
-  joinAndBox('-'),
+  R.join('-'),
   splitAndBox(' '));
 
 assert.equals(toSlug("Functional Fun"), "functional-fun");
